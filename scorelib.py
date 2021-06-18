86ff6a6ec045ff947fa869e54507819e39a56dde
Ted Skyvell <tskyvell@gmail.com>
2021-06-18 14:56:06 +0000

    First commit.

--- scorelib.py
+++ scorelib.py
@@ -1,4 +1,5 @@
 from iconservice import *
+from .scorelib.reward_handler import RewardHandler
 
 TAG = 'SCORELib'
 
@@ -7,13 +8,14 @@ class SCORELib(IconScoreBase):
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
+        self._reward_handler = RewardHandler("rewards", 18, db, self)
 
     def on_install(self) -> None:
         super().on_install()
-
+        
     def on_update(self) -> None:
         super().on_update()
-    
+
     @external(readonly=True)
     def hello(self) -> str:
         Logger.debug(f'Hello, world!', TAG)
--- tests/test_unit_scorelib.py
+++ tests/test_unit_scorelib.py
@@ -7,6 +7,8 @@ class TestSCORELib(ScoreTestCase):
     def setUp(self):
         super().setUp()
         self.score = self.get_score_instance(SCORELib, self.test_account1)
+        self.reward_handler = self.score._rewards
 
-    def test_hello(self):
-        self.assertEqual(self.score.hello(), "Hello")
+    def test_distribute_rewards(self):
+        self.reward_handler.distribute_rewards(10, 10000)
+        print(self.reward_handler.query_rewards(self.test_account1, 20))
